d3c0b506b73a50725efdfac32123871f2c8254ff
nia-h <nia.hq@yahoo.com>
2023-11-06 14:54:42 +0800

    chore: add image_storage_URL to env

--- server/api.ts
+++ server/api.ts
@@ -1,21 +1,21 @@
-import express from 'express';
-import cors from 'cors';
-import { initTRPC } from '@trpc/server';
-import { createExpressMiddleware } from '@trpc/server/adapters/express';
+import express from "express";
+import cors from "cors";
+import { initTRPC } from "@trpc/server";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5177' }));
+app.use(cors({ origin: "http://localhost:5177" }));
 
 const t = initTRPC.create();
 
 const appRouter = t.router({
   sayHi: t.procedure.query(() => {
-    return 'Hi';
+    return "Hi";
   }),
   logToServer: t.procedure
     .input(v => {
-      if (typeof v === 'string') return v;
-      throw new Error('Invalid input: expected string ');
+      if (typeof v === "string") return v;
+      throw new Error("Invalid input: expected string ");
     })
     .mutation(req => {
       console.log(`client says ${req.input} `);
@@ -24,7 +24,7 @@ const appRouter = t.router({
 });
 
 //@ts-ignore
-app.use('/trpc', createExpressMiddleware({ router: appRouter }));
+app.use("/trpc", createExpressMiddleware({ router: appRouter }));
 
 app.listen(3000);
 
